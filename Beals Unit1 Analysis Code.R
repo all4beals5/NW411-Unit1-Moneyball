09423fdb360fcef0d0a34b0dfa0dd1d8074ccedc
Brandi Beals <brandi.beals@gmail.com>
2018-09-28 21:19:35 -0500

    fielding EDA

--- Beals Unit1 Analysis Code.R
+++ Beals Unit1 Analysis Code.R
@@ -15,7 +15,29 @@ moneyball=read.csv("moneyball.csv",header=T)
 ### Exploratory data analysis
 str(moneyball)
 summary(moneyball)
-head(moneyball)
+
+hist(moneyball$TARGET_WINS, main="Likely Values of Target Wins", breaks=30) # know the range of likely values
+par(mar=c(5, 10, 4, 2) + 0.1)
+boxplot(moneyball[3:17], horizontal=TRUE,las=2) # get a visual sense for values of predictor variables
+
+### Fielding
+par(mfrow=c(2,2), mai=c(0.5,0.5,0.5,0.2))
+par(fig=c(0,0.5,0.25,1))
+hist(moneyball$TEAM_FIELDING_E, main="Fielding Errors", breaks=30, col="firebrick")
+par(fig=c(0.5,1,0.25,1), new=TRUE)
+hist(moneyball$TEAM_FIELDING_DP, main="Double Plays", breaks=30, col="seagreen")
+par(fig=c(0,0.5,0,0.3), new=TRUE)
+boxplot(moneyball$TEAM_FIELDING_E, horizontal=TRUE, width=1, col="firebrick")
+par(fig=c(0.5,1,0,0.3), new=TRUE)
+boxplot(moneyball$TEAM_FIELDING_DP, horizontal=TRUE, width=1, col="seagreen")
+par(mfrow=c(1,1))
+
+### Baserunning
+
+### Pitching
+
+### Batting
+
 
 ##################################################
 ### Preparation and transformations
