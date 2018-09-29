c568e35d322feaa9f1f5d7c4591be0dcd6ef5dc6
Brandi Beals <brandi.beals@gmail.com>
2018-09-28 20:31:12 -0500

    set working directory, read file

---
+++ Beals Unit1 Analysis Code.R
@@ -0,0 +1,36 @@
+##################################################
+### Unit 1 Assignment – Moneyball
+### MSDS 411-DL-56
+### Brandi Beals
+
+##################################################
+### Install packages
+
+##################################################
+### Set working directory & read data
+setwd("C:/Users/Brara/Dropbox/Masters in Predictive Analytics/411-DL-56/W-unit 1 Weeks 1 to 3/Unit 1 - Moneyball/4 Homework")
+moneyball=read.csv("moneyball.csv",header=T)
+
+##################################################
+### Exploratory data analysis
+str(moneyball)
+summary(moneyball)
+head(moneyball)
+
+##################################################
+### Preparation and transformations
+
+##################################################
+### Model creation
+
+##################################################
+### Model selection
+
+##################################################
+### 
+
+##################################################
+### 
+
+##################################################
+### 
