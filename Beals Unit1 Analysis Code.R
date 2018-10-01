e55f1b9e06a3f5087da1e65d94556c0c6fe08547
Brandi Beals <brandi.beals@gmail.com>
2018-09-30 20:37:51 -0500

    full model

--- Beals Unit1 Analysis Code.R
+++ Beals Unit1 Analysis Code.R
@@ -21,6 +21,7 @@ summary(moneyball)
 hist(moneyball$TARGET_WINS, main="Likely Values of Target Wins", breaks=30) # know the range of likely values
 par(mar=c(5, 10, 4, 2) + 0.1)
 boxplot(moneyball[3:17], horizontal=TRUE,las=2) # get a visual sense for values of predictor variables
+plot(moneyball[3:17])
 
 ### Defense
 ### Fielding
@@ -120,13 +121,19 @@ boxplot(Singles, horizontal=TRUE, width=1, col="seagreen")
 par(mfrow=c(1,1))
 
 ### Correlation matrix
-corrplot(cor(moneyball[3:17]), method="color", type="upper", tl.col="black", tl.cex=.7, addCoef.col="black", number.cex=.8)
+### NAs cause issues
+corrplot(cor(moneyball[3:17]), method="color", type="upper", tl.col="black", tl.cex=.7, 
+         addCoef.col="black", number.cex=.8)
+### NAs removed
+corrplot(cor(moneyball[3:17], use="complete.obs"), method="color", type="upper", tl.col="black", tl.cex=.7, 
+         addCoef.col="black", number.cex=.8)
+
 chart.Correlation(moneyball[3:17])
 
 ##################################################
 ### Preparation and transformations
 
-### Imputation
+### Recoding NAs to zero
 moneyballzero <- moneyball
 moneyballzero$TEAM_BATTING_SO[is.na(moneyballzero$TEAM_BATTING_SO)==TRUE] <- 0
 moneyballzero$TEAM_BASERUN_SB[is.na(moneyballzero$TEAM_BASERUN_SB)==TRUE] <- 0
@@ -136,10 +143,28 @@ moneyballzero$TEAM_PITCHING_SO[is.na(moneyballzero$TEAM_PITCHING_SO)==TRUE] <- 0
 moneyballzero$TEAM_FIELDING_DP[is.na(moneyballzero$TEAM_FIELDING_DP)==TRUE] <- 0
 summary(moneyballzero)
 
-### 
+### NAs set to zero
+corrplot(cor(moneyballzero[3:17]), method="color", type="upper", tl.col="black", tl.cex=.7, 
+         addCoef.col="black", number.cex=.8)
+
+chart.Correlation(moneyballzero[3:11])
+chart.Correlation(moneyball[3:11])
+chart.Correlation(moneyballzero[12:17])
+chart.Correlation(moneyball[12:17])
+
+### Normalize
 
 ##################################################
 ### Model creation
+fullmodel <- lm(TARGET_WINS ~ TEAM_BATTING_H+TEAM_BATTING_2B+TEAM_BATTING_3B+TEAM_BATTING_HR+
+     TEAM_BATTING_BB+TEAM_BATTING_HBP+TEAM_BATTING_SO+TEAM_BASERUN_SB+TEAM_BASERUN_CS+
+     TEAM_PITCHING_H+TEAM_PITCHING_HR+TEAM_PITCHING_BB+TEAM_PITCHING_SO+
+     TEAM_FIELDING_E+TEAM_FIELDING_DP, data=moneyball)
+
+fullmodelzero <- lm(TARGET_WINS ~ TEAM_BATTING_H+TEAM_BATTING_2B+TEAM_BATTING_3B+TEAM_BATTING_HR+
+                  TEAM_BATTING_BB+TEAM_BATTING_HBP+TEAM_BATTING_SO+TEAM_BASERUN_SB+TEAM_BASERUN_CS+
+                  TEAM_PITCHING_H+TEAM_PITCHING_HR+TEAM_PITCHING_BB+TEAM_PITCHING_SO+
+                  TEAM_FIELDING_E+TEAM_FIELDING_DP, data=moneyballzero)
 
 ##################################################
 ### Model selection
